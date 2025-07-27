724e4eebea3a40412486723de8cb0c900f7807d3
Daniel Portillo <daniel.portillo@farmaciasperuanas.pe>
2025-07-27 01:18:54 -0500

    add workflow and terraform structure

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.44.0"
+    }
+  }
+
+  required_version = ">= 1.5.0"
+}
+
 provider "aws" {
   region = var.aws_region
 }
@@ -7,6 +18,7 @@ resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
+
   tags = {
     Name = "eks-vpc"
   }
@@ -18,6 +30,7 @@ resource "aws_subnet" "public_a" {
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "${var.aws_region}a"
   map_public_ip_on_launch = true
+
   tags = {
     Name = "public-a"
   }
@@ -28,6 +41,7 @@ resource "aws_subnet" "public_b" {
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "${var.aws_region}b"
   map_public_ip_on_launch = true
+
   tags = {
     Name = "public-b"
   }
@@ -38,6 +52,7 @@ resource "aws_subnet" "private_a" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.3.0/24"
   availability_zone = "${var.aws_region}a"
+
   tags = {
     Name = "private-a"
   }
@@ -47,6 +62,7 @@ resource "aws_subnet" "private_b" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.4.0/24"
   availability_zone = "${var.aws_region}b"
+
   tags = {
     Name = "private-b"
   }
@@ -54,23 +70,22 @@ resource "aws_subnet" "private_b" {
 
 # EKS Cluster
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "20.8.4"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.4.0"  # Estable, evita errores con bloques no soportados
 
   cluster_name    = "demo-eks-cluster"
   cluster_version = "1.29"
+  vpc_id          = aws_vpc.main.id
   subnet_ids      = [
     aws_subnet.private_a.id,
     aws_subnet.private_b.id
   ]
-  vpc_id          = aws_vpc.main.id
 
   eks_managed_node_groups = {
     default = {
-      desired_size = 2
-      max_size     = 2
-      min_size     = 1
-
+      desired_size   = 2
+      min_size       = 1
+      max_size       = 2
       instance_types = ["t3.medium"]
     }
   }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,10 +2,10 @@ output "cluster_name" {
   value = module.eks.cluster_name
 }
 
-output "kubeconfig" {
-  value = module.eks.kubeconfig
-  sensitive = true
-}
+#output "kubeconfig" {
+  #value = module.eks.kubeconfig
+  #sensitive = true
+#}
 
 output "eks_cluster_endpoint" {
   value = module.eks.cluster_endpoint
