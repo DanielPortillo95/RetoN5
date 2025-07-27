df0e8dca175d56e2f6f1c67f5ad55a72c477cf73
Daniel Portillo <daniel.portillo@farmaciasperuanas.pe>
2025-07-27 10:34:19 -0500

    update

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -25,7 +25,7 @@ resource "aws_eks_node_group" "this" {
     min_size     = 1
   }
 
-  instance_types = ["t2.micro"]
+  instance_types = ["t3.micro"]
 
   tags = {
     Name = "${var.cluster_name}-nodes"
