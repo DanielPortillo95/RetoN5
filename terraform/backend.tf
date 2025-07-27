3aa9eda99b25efe75be4bb4f626b9292afa8224f
Daniel Portillo <daniel.portillo@farmaciasperuanas.pe>
2025-07-27 12:33:13 -0500

    update s3

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket         = "tfstate-n5-dportillo"
     key            = "eks/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-lock" # si lo usas
     encrypt        = true
   }
 }
