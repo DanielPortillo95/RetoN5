1e83aae23de55321626a715f64fe70571f1a1939
Daniel Portillo <daniel.portillo@farmaciasperuanas.pe>
2025-07-27 12:16:32 -0500

    update main.tf nat

--- terraform/main.tf
+++ terraform/main.tf
@@ -78,10 +78,6 @@ resource "aws_internet_gateway" "igw" {
   tags = { Name = "igw" }
 }
 
-resource "aws_eip" "nat" {
-  vpc = true
-}
-
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public_a.id
