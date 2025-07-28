d88a467683fed7acfd485c6789fe88d04a182299
Daniel Portillo <daniel.portillo@farmaciasperuanas.pe>
2025-07-28 06:52:32 -0500

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,10 +16,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-data "aws_eks_cluster" "eks" {
-  name = module.eks.cluster_name
-}
-
 data "aws_eks_cluster_auth" "eks" {
   name = module.eks.cluster_name
 }
