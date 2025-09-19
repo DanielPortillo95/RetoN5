85be19ba06724846f981e0841d812e7334df93ad
DanielPortillo95 <77551857+DanielPortillo95@users.noreply.github.com>
2025-09-18 23:01:46 -0500

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "tfstate-n5-dportillo"
+    bucket         = "tfstate-n5-dportillos"
     key            = "eks/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
